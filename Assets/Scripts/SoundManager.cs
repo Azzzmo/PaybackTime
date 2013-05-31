bcf84bdf63b8ec749ec4c15c9684df0264fa75d3
Tuukka Teppola <tuukka@teppola.fi>
2013-05-31 14:38:57 +0300

    Updated SoundManager.cs

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -5,7 +5,7 @@ using System.Collections;
 public class SoundManager : MonoBehaviour {
 	
 	//public CharacterSounds[] CharacterAudioSources;
-	private List<Transform> CharacterAudioSources = new List<Transform>();
+	private List<SoundObject> soundObjects = new List<SoundObject>();
 	
 	public int minWaitTimeBetweenClips = 0;
 	public int maxWaitTimeBetweenClips = 10;
@@ -15,9 +15,10 @@ public class SoundManager : MonoBehaviour {
 	{
 		foreach(GameObject go in GameObject.FindGameObjectsWithTag("Player"))
 		{
-			if(go.GetComponent<CharacterSounds>() != null)
+			CharacterSounds CS = go.GetComponent<CharacterSounds>();
+			if(CS != null)
 			{
-				CharacterAudioSources.Add(go.transform);
+				soundObjects.Add(new SoundObject(go.GetComponent<CharacterBase>().mystate, CS));
 			}
 		}
 		
@@ -25,7 +26,7 @@ public class SoundManager : MonoBehaviour {
 		{
 			if(go.GetComponent<CharacterSounds>() != null)
 			{
-				CharacterAudioSources.Add(go.transform);
+				soundObjects.Add(new SoundObject(go.GetComponent<CharacterBase>().mystate, CS));
 			}
 		}
 		
@@ -34,21 +35,24 @@ public class SoundManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		foreach(Transform tr in CharacterAudioSources)
-		{
-			CharacterBase CB = tr.GetComponent<CharacterBase>();
-			CharacterSounds CS = tr.GetComponent<CharacterSounds>();
-			
-			if(CB.mystate == AnimState.Idle || CB.mystate == AnimState.Running || CB.mystate == AnimState.Walking)
+		foreach(SoundObject so in soundObjects)
+		{			
+			if(so.soundState == AnimState.Idle || so.soundState == AnimState.Running || so.soundState == AnimState.Walking)
 			{
-				CS.waitingToPlay = true;
-				CS.Invoke("PlayIdleClip", Random.Range(minWaitTimeBetweenClips, maxWaitTimeBetweenClips));
+				if(so.sounds != null && !so.sounds.audio.isPlaying && !so.sounds.waitingToPlay)
+				{
+					so.sounds.waitingToPlay = true;
+					so.sounds.Invoke("PlayIdleClip", Random.Range(minWaitTimeBetweenClips, maxWaitTimeBetweenClips));
+				}
 			}
 			
-			else if(CB.mystate == AnimState.Attack || CB.mystate == AnimState.Breaking)
+			else if(so.soundState == AnimState.Attack || so.soundState == AnimState.Breaking)
 			{
-				CS.waitingToPlay = true;
-				CS.Invoke("PlayCombatClip", Random.Range(minWaitTimeBetweenClips, maxWaitTimeBetweenClips));
+				if(so.sounds != null && !so.sounds.audio.isPlaying && !so.sounds.waitingToPlay)
+				{
+					so.sounds.waitingToPlay = true;
+					so.sounds.Invoke("PlayCombatClip", Random.Range(minWaitTimeBetweenClips, maxWaitTimeBetweenClips));
+				}
 			}
 			
 			
@@ -63,4 +67,17 @@ public class SoundManager : MonoBehaviour {
 		}
 		
 	}
+}
+
+public class SoundObject
+{
+	public AnimationState soundState;
+	public CharacterSounds sounds;
+	
+	public SoundObject(AnimationState state, CharacterSounds CS)
+	{
+		soundState = state;
+		sounds = CS;
+	}
+	
 }
