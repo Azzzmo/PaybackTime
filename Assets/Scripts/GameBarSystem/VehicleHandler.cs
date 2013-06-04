fa45f659bdd2da0d55f59968aa54d68202fa6457
Tuukka Teppola <tuukka@teppola.fi>
2013-06-04 16:15:56 +0300

    Added kuebelwagen.

--- Assets/Scripts/GameBarSystem/VehicleHandler.cs
+++ Assets/Scripts/GameBarSystem/VehicleHandler.cs
@@ -1,28 +1,51 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class VehicleHandler : MonoBehaviour {
 	
 	ControlObjHandler COH;
+	List<Transform> charactersIn = new List<Transform>();
 
 	// Use this for initialization
 	void Start () {
 		COH = this.GetComponent<ControlObjHandler>();
-		//COH.enabled = false;
+		COH.enabled = false;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+		if(Input.GetButtonUp("Jump") && charactersIn.Count > 0)
+		{
+			int i = 5;
+			
+			foreach(Transform tr in charactersIn)
+			{
+				tr.position = new Vector3(i,0,i) + this.transform.position;
+				tr.parent = null;	
+				i += 2;
+			}
+			
+			setControlling(false);
+		}
 	
 	}
 	
-	public void EnableControlling()
+	public void setControlling(bool setting)
 	{
-		COH.enabled = true;
+		COH.enabled = setting;
 	}
 	
 	public void setMovable(bool movable)
 	{
 		COH.Moveable = movable;	
+	}
+	
+	public void makeChild(Transform child)
+	{		
+		child.parent = this.transform;
+		child.position = new Vector3(0, -100, 0);
+		charactersIn.Add(child);
 	}
 }
--- Assets/Scripts/InteractionHandler.cs
+++ Assets/Scripts/InteractionHandler.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 using System.Collections;
 
 public class InteractionHandler : MonoBehaviour {
@@ -8,6 +9,8 @@ public class InteractionHandler : MonoBehaviour {
 	
 	private Transform enemyTarget;
 	private VehicleHandler wagen;
+	
+	private List<MoveToCarObj> moveToCar = new List<MoveToCarObj>();
 
 	// Use this for initialization
 	void Start () 
@@ -31,15 +34,8 @@ public class InteractionHandler : MonoBehaviour {
 					
 					if(TargetCastInfo.transform.tag == "Car")
 					{
-						if(CB.canDrive)
-						{
-							print("Kuski käsketty!");
-							selectionBehaviour.CaculateNewPathForOne(transform, TargetCastInfo.transform.position);
-						}
-						else
-						{
-							print ("Kyytiläinen käsketty!");
-						}
+						print("Kuski käsketty!");
+						moveToCar.Add(new MoveToCarObj(trans, CB, TargetCastInfo.transform.position));
 					}
 					else
 					{
@@ -48,6 +44,57 @@ public class InteractionHandler : MonoBehaviour {
 				}
 			}
 		}
+		
+		foreach(MoveToCarObj mtco in moveToCar)
+		{
+			MoveTo(mtco);
+		}
 
+	}
+	
+	void MoveTo(MoveToCarObj MTCO)
+	{
+		//move character to range
+		MTCO.ownTrans.LookAt(new Vector3(MTCO.ownMoveTo.x, MTCO.ownTrans.position.y, MTCO.ownMoveTo.z));
+		Vector3 moveDirection = MTCO.ownMoveTo - MTCO.ownTrans.position;//this.transform.forward;
+		moveDirection.Normalize();
+		moveDirection *= MTCO.ownCB.maxspeed;
+		
+		if(Vector3.Distance(new Vector3(MTCO.ownMoveTo.x, 0f, MTCO.ownMoveTo.z), new Vector3(MTCO.ownTrans.position.x, 0f, MTCO.ownTrans.position.z)) > MTCO.ownCB.meleeDistance +3)
+		{
+			MTCO.ownCB.mystate = AnimState.Walking;
+			//moveDirection.y -= gravity * Time.deltaTime;
+			MTCO.ownTrans.GetComponent<CharacterController>().Move(moveDirection * MTCO.ownCB.maxspeed * Time.deltaTime);
+		}
+		//if in range, attack
+		else if(Vector3.Distance(new Vector3(MTCO.ownMoveTo.x, 0f, MTCO.ownMoveTo.z), new Vector3(MTCO.ownTrans.position.x, 0f, MTCO.ownTrans.position.z)) <= MTCO.ownCB.meleeDistance +3)
+		{
+			MTCO.movingTo = false;
+			moveToCar.Remove(MTCO);
+			print ("Auton vieressä!");
+			
+			wagen.makeChild(MTCO.ownTrans);
+			
+			if(MTCO.ownCB.canDrive)
+				wagen.setControlling(true);
+		}	
+	}
+	
+
+}
+
+public class MoveToCarObj
+{
+	public Transform ownTrans;
+	public CharacterBase ownCB;
+	public Vector3 ownMoveTo;
+	public bool movingTo;
+	
+	public MoveToCarObj(Transform trans, CharacterBase CB, Vector3 moveTo)
+	{
+		ownTrans = trans;
+		ownCB = CB;
+		ownMoveTo = moveTo;
+		movingTo = true;
 	}
 }
